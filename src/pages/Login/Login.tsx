0488ebb7408471c29bb18ae1458113a2eae2c83b
copet80 <copet80@gmail.com>
2022-08-08 02:08:19 +0000

    Added login

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container, Heading } from '@chakra-ui/react';
+
+export default function App() {
+  return (
+    <Container>
+      <Heading>Taecomps</Heading>
+      <Heading size="md">Organised Competitions</Heading>
+    </Container>
+  );
+}
